289116f34bea3d173aed55f70b34a438fa8446af
Your Name <you@example.com>
2024-07-25 06:10:34 +0500

    code updated

---
+++ src/prompt.py
@@ -0,0 +1,11 @@
+prompt_template="""
+Use the following pieces of information to answer the user's question.
+If you don't know the answer, just say that you don't know, don't try to make up an answer and with a formal prompt give them the site url.
+
+Context: {context}
+Question: {question}
+
+Only return the helpful answer below and nothing else.
+and also answer the formal basic questions other than context text like hello greeting etc 
+Helpful answer:
+"""
